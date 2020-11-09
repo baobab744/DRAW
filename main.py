cb2678d1cfd5ecae4b72d3a5bbc60e6ae4474d7b
baobab744 <baobab744@yandex.ru>
2020-11-09 17:26:07 +1000

    1

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-WIDTH_WIN, HEIGHT_WIN = 400, 400
+WIDTH_WIN, HEIGHT_WIN = 800, 600
 collide = False
+collide1 = False
 num = 0
-block = False
+num1 = 0
+speed_x, speed_y = 5, 5
+FPS = 120
+clock = pygame.time.Clock()
 
 # Квадрат
 rect_size = w, h = 70, 70
@@ -32,26 +36,49 @@ surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA
 pygame.draw.circle(surface, YELLOW, (circle_radius, circle_radius), circle_radius)
 rect1 = surface.get_rect()
 
+ball = pygame.image.load('img/ball.png')
+ball_rect = ball.get_rect(topleft=(0, 0))
+print(ball_rect)
+
 run = True
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
         elif e.type == pygame.MOUSEMOTION:
-            # circle_pos = e.pos
             rect1.center = e.pos
-
+    ball_rect = ball_rect.move(speed_x, speed_y)
+    if ball_rect.left < 0 or ball_rect.right > WIDTH_WIN:
+        speed_x = -speed_x
+    if ball_rect.top < 0 or ball_rect.bottom > HEIGHT_WIN:
+        speed_y = -speed_y
     screen.fill(BG)
+
+    COLOR = RED if collide or collide1 else BLUE
+
+    def move(speed_x, speed_y)
+        if ball_rect.left < 0 or ball_rect.right > WIDTH_WIN:
+            speed_x = -speed_x
+        if ball_rect.top < 0 or ball_rect.bottom > HEIGHT_WIN:
+            speed_y = -speed_y
 
     rect2 = pygame.draw.rect(screen, RED if collide else BLUE, (rect_pos, rect_size))
     screen.blit(surface, rect1)
-    screen.blit(font.render(str(num), True, BLACK), (10, 0))
+    screen.blit(ball, ball_rect)
+    screen.blit(font.render(str(num), True, BLACK), (40, 25))
+    screen.blit(font.render(str(num1), True, BLACK), (WIDTH_WIN - 40, 25))
     if rect1.colliderect(rect2):
         collide = True
-        if not block:
+        if COLOR == BLUE:
             num += 1
-            block = True
     else:
         collide = False
+    if ball_rect.colliderect(rect2):
+        collide1 = True
+        if COLOR == BLUE:
+            num1 += 1
+    else:
+        collide1 = False
 
     pygame.display.update()
+    clock.tick(FPS)
